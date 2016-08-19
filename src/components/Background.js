c96929d96645159103759e3e87e28aa2cc85a8ea
AndreasRadloff <andreas@radloff.se>
2016-08-19 17:21:22 +0200

    speak

--- src/components/Background.js
+++ src/components/Background.js
@@ -4,7 +4,8 @@ var EventEmitterMixin = require('react-event-emitter-mixin');
 const Background = React.createClass({
   mixins:[EventEmitterMixin],
   handleClick: function(event) {
-    this.eventEmitter('emit','walkTo',event.clientX,event.clientY);
+    this.eventEmitter('emit','walkTo',event.screenX,event.screenY);
+    this.eventEmitter('emit','speak','Här går man');
   },
   render: function(){
     return (<img onClick={this.handleClick} className="background" src={this.props.file}/>);
