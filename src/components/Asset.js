ba8db907f9088f991d4066174c1ffb46bb5da12f
Johan Schierbeck <johan@schilie.se>
2016-08-19 17:48:03 +0200

    Updates in assets.

--- src/components/Asset.js
+++ src/components/Asset.js
@@ -1,11 +1,21 @@
 import React from 'react';
-
+var EventEmitterMixin = require('react-event-emitter-mixin');
 const Asset = React.createClass({
 
 	getInitialState: function() {
 		var id = this.props.assetId;
 		return require('json!../assets/'+id);
 	},
+
+	mixins:[EventEmitterMixin],
+	handleClick: function(event) {
+		eval(this.state.actions.view)	  
+	},
+
+	msg: function(text) {
+		this.eventEmitter('emit','speak',text);
+	},
+
   render: function(){
   	var inlineStyle = {
   	  display: this.state.on_stage == true ? 'initial' : 'none',
