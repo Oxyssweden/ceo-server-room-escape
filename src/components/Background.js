e614a3104ac024ebc1c9438cc424dd9abbb3f70f
AndreasRadloff <andreas@radloff.se>
2016-08-19 21:57:11 +0200

    limit movement to floor

--- src/components/Background.js
+++ src/components/Background.js
@@ -4,7 +4,10 @@ var EventEmitterMixin = require('react-event-emitter-mixin');
 const Background = React.createClass({
   mixins:[EventEmitterMixin],
   handleClick: function(event) {
-    this.eventEmitter('emit','walkTo',event.screenX,event.screenY);
+    var floor = 520;
+    if(event.screenY > floor) {
+      this.eventEmitter('emit','walkTo',event.screenX,event.screenY);
+    };
   },
   render: function(){
     return (<img onClick={this.handleClick} className="background" src={this.props.file}/>);
