de2ea2365768370a1df5ffa6eed4263e00991722
Johan Schierbeck <johan@schilie.se>
2016-08-19 15:06:53 +0200

    Asset and asset handler.

--- src/components/Asset.js
+++ src/components/Asset.js
@@ -1,8 +1,28 @@
 import React from 'react';
 
 const Asset = React.createClass({
+
+	getInitialState: function(){
+	  return {
+	    initialAssets: [
+	      "broom",
+	      "cabinet",
+	      "cable",
+	      "newspaper",
+	      "waterpipe"
+	    ],
+	    assets: []
+	  }
+	},
+	componentWillMount: function(){
+	  this.setState({assets: this.state.initialAssets})
+	},
+
   render: function(){
-    return (<div className="asset">{this.props.name}</div>);
+    return (<div className="asset">
+    	{this.props.name}
+    	{this.props.sprite}
+    </div>);
   }
 });
 
