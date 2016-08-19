8c65939eee2d59e08c1931b8c26f8a362894425c
AndreasRadloff <andreas@radloff.se>
2016-08-19 21:47:37 +0200

    fixed walking

--- src/components/Me.js
+++ src/components/Me.js
@@ -5,22 +5,26 @@ const Me = React.createClass({
   timer: null,
   walkingInterval: null,
   mixins:[EventEmitterMixin],
+
   getInitialState: function(){
     return {
       sprite: '/images/me/standing-down.gif',
       top: 350,
       left: 100,
       height: 350,
-      width: 200
+      width: 200,
+      speed: 10
     }
   },
+
   componentWillMount(){
     this.eventEmitter('on','walkTo',(x,y)=>{
+      this.stopWalk();
       this.setState({
         destinationLeft: x - (this.state.width/2),
         destinationTop: y - this.state.height
       });
-      this.walkingInterval = setInterval(this.walk, 100);
+      this.walkingInterval = setInterval(this.walk, 30);
     });
     this.eventEmitter('on','speak',(text)=>{
       this.stopWalk();
@@ -31,36 +35,58 @@ const Me = React.createClass({
       this.timer = this.setTimer(this.stopSpeak, text.length / 10 * 1000);
     });
   },
+
   setTimer(func, millis) {
     clearTimeout(this.timer);
     setTimeout(func, millis);
   },
+
   stopSpeak: function() {
     this.setState({
       sprite: '/images/me/standing-down.gif',
       saying: false,
     });
   },
+
   stopWalk: function() {
     clearInterval(this.walkingInterval);
     this.setState({
       sprite: '/images/me/standing-down.gif',
     });
   },
+
   walk: function() {
-    if (Math.round(this.state.top) == this.state.destinationTop && Math.round(this.state.left) == this.state.destinationLeft) {
+    var
+      top = this.state.top,
+      left = this.state.left,
+      destinationTop = this.state.destinationTop,
+      destinationLeft = this.state.destinationLeft,
+      distanceTop = destinationTop - top,
+      distanceLeft = destinationLeft - left,
+      distance = Math.sqrt(Math.pow(distanceTop,2)+Math.pow(distanceLeft,2)),
+      speed = this.state.speed,
+      directionTop = (destinationTop-top) / distance,
+      directionLeft = (destinationLeft-left) / distance,
+      direction = directionLeft < 0 ? 'left' : 'right',
+      moveTop = directionTop * speed,
+      moveLeft = directionLeft * speed;
+
+    if (distance < speed) {
       this.stopWalk();
+      this.setState({
+        top: destinationTop,
+        left: destinationLeft,
+      });
       return;
     }
 
-    var newTop = this.state.top + ((this.state.destinationTop - this.state.top) / 10);
-    var newLeft = this.state.left + ((this.state.destinationLeft - this.state.left) / 10);
     this.setState({
-      top: newTop,
-      left: newLeft,
-      sprite: '/images/me/walk-left.gif',
+      top: this.state.top + moveTop,
+      left: this.state.left + moveLeft,
+      sprite: '/images/me/walk-' + direction + '.gif',
     });
   },
+  
   render: function() {
     var meStyle = {
       top: this.state.top,
