27d810abb7eb192e9d5d5b4919c2e8650f8dfa8d
AndreasRadloff <andreas@radloff.se>
2016-08-20 21:08:35 +0200

    images in inventory

--- src/components/List.js
+++ src/components/List.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 const List = React.createClass({
+  handleClick: function(event) {
+    say('Not implemented yet...');
+  },
   render: function(){
+    var that = this;
     return (
       <ul class="list">
         {
           this.props.items.map(function(item) {
-            return <li key={item.state.id}>{item.state.id}</li>
+            return <li onClick={that.handleClick} key={item.state.id}><img  className="inventory-item" src={item.state.sprite}/></li>
           })
         }
       </ul>
