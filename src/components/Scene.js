6111d96909929220e6534b04d5b4272083c8241f
Johan Schierbeck <johan@whoop.se>
2016-08-22 09:06:38 +0200

    Images changed and assets added

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -9,18 +9,26 @@ const Scene = React.createClass({
   render: function(){
     return (<div id="scene">
       <Background file="/images/room.svg"/>
+      <Asset assetId="binder_shelf"/>
       <Asset assetId="broom"/>
       <Asset assetId="broomandknife"/>
+      <Asset assetId="business_card"/>
       <Asset assetId="cabinet"/>
       <Asset assetId="cable"/>
+      <Asset assetId="calendar"/>
+      <Asset assetId="computer_table"/>
+      <Asset assetId="copying_machine"/>
       <Asset assetId="key"/>
       <Asset assetId="knife"/>
       <Asset assetId="knifebroom"/>
+      <Asset assetId="lamp"/>
       <Asset assetId="newspaper"/>
-      <Asset assetId="waterpipe"/>
-      <Asset assetId="waterpipe_fixed"/>
+      <Asset assetId="paper_shelf"/>
+      <Asset assetId="paper_shredder"/>
       <Asset assetId="rack"/>
       <Asset assetId="servers"/>
+      <Asset assetId="waterpipe"/>
+      <Asset assetId="waterpipe_fixed"/>
       <Me/>
       <Foreground file="/images/foreground.svg"/>
       <ContextMenu/>
